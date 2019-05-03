265e172f1b8e2edfe124a8e99efc0da1a72d134d
CesarMateos <cmateos@est.frba.utn.edu.ar>
2019-05-03 00:27:15 -0300

    Clase AutoManejable y Camaras

---
+++ TGC.Group/Model/AutoManejable.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.DirectX.DirectInput;
+using System.Drawing;
+using TGC.Core.Direct3D;
+using TGC.Core.Example;
+using TGC.Core.Camara;
+using TGC.Core.Geometry;
+using TGC.Core.Input;
+using TGC.Core.Mathematica;
+using TGC.Core.SceneLoader;
+using TGC.Core.Textures;
+
+namespace TGC.Group.Model
+{
+    public class AutoManejable
+    {
+        private TgcMesh maya;
+        public TgcMesh Maya { get => maya; set => maya = value; }
+        public AutoManejable(TgcMesh valor)
+        {
+            maya = valor;
+        }
+        public float gradosGiro = 0.017f;
+        public float velocidadMinima = -2;
+        public float velocidadMaxima = 13;
+        private float Aceleracion { get; set; }
+        public int Direccion { get; set; }
+        public float Grados { get; set; }
+        private float velocidad;
+        public float Velocidad
+        {
+            get => FastMath.Min(FastMath.Max(Aceleracion * Direccion, velocidadMinima), velocidadMaxima);
+            set => velocidad = value;
+        }
+
+        public TGCVector3 versorDirector()
+        {
+            return new TGCVector3(FastMath.Cos(4.71238898f + Grados), 0, FastMath.Sin(4.71238898f + Grados));
+        }
+
+        public float giroTotal()
+        {
+            return gradosGiro * (Velocidad / 10);
+        }
+
+        //Movimiento
+        public void acelera()
+        {
+            Aceleracion += 0.02f;
+            Direccion = 1;
+        }
+        public void frena()
+        {
+            Aceleracion -= 0.1f;
+            if (Velocidad < 1f)
+            {
+                Aceleracion = 0;
+            }
+        }
+        public void marchaAtras()
+        {
+            Aceleracion += 0.02f;
+            Direccion = -1;        
+        }
+        public void giraDerecha()
+        {
+            Grados -= this.giroTotal();
+            Maya.RotateY(+giroTotal());
+        }
+        public void giraIzquierda()
+        {
+            Grados += this.giroTotal();
+            Maya.RotateY(-giroTotal());
+        }
+        public void parado()
+        {
+            Maya.RotateY(0);
+            if (Aceleracion != 0)
+            {
+                if (Aceleracion > 0.05f)
+                {
+                    Aceleracion -= 0.008f;
+                }
+                else if (Aceleracion < -0.05f)
+                {
+                    Aceleracion += 0.008f;
+                }
+                else
+                {
+                    Aceleracion = 0;
+                }
+            }
+        }
+        public void moverse()
+        {
+            maya.Move(this.versorDirector() * Velocidad);
+        }
+    }
+
+
+
+}
+
+
---
+++ TGC.Group/Model/Camaras.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.DirectX.DirectInput;
+using System.Drawing;
+using TGC.Core.Direct3D;
+using TGC.Core.Example;
+using TGC.Core.Camara;
+using TGC.Core.Geometry;
+using TGC.Core.Input;
+using TGC.Core.Mathematica;
+using TGC.Core.SceneLoader;
+using TGC.Core.Textures;
+
+namespace TGC.Group.Model
+{
+
+		public class CamaraAtras : TgcCamera{
+			public AutoManejable objetivo;
+            public CamaraAtras(AutoManejable nuevo_objetivo)
+                {
+                    objetivo = nuevo_objetivo;
+                    this.SetCamera(posicionCamaraAtras, objetivo.Maya.Position);
+                }
+            public float distanciaCamaraAtras = 200;
+            public float alturaCamaraAtras = 50;
+            private float lambda;
+            public float Lambda { get => distanciaCamaraAtras / FastMath.Sqrt((FastMath.Pow2(objetivo.versorDirector().X)) + FastMath.Pow2(objetivo.versorDirector().Z)); set => lambda = value; }
+            private TGCVector3 posicionCamaraAtras;
+            public TGCVector3 PosicionCamaraAtras { get => new TGCVector3(objetivo.Maya.Position.X - (lambda * objetivo.Direccion * objetivo.versorDirector().X), alturaCamaraAtras, objetivo.Maya.Position.Z - (lambda * objetivo.Direccion * objetivo.versorDirector().Z)); set => posicionCamaraAtras = value; }
+
+
+    }
+    public class CamaraAerea : TgcCamera{
+			
+			public TgcMesh objetivo;
+            TGCVector3 posicionCamaraArea = new TGCVector3(50, 2900, 0);
+                public CamaraAerea(TgcMesh nuev_objetivo)
+                {
+                    objetivo = nuev_objetivo;
+                    this.SetCamera(posicionCamaraArea, objetivo.Position);
+                }
+			
+		}
+		
+}
